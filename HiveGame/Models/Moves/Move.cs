e85ad8610f7e77c1ab1acfcf400005d0db2a4391
Daniel Jonker <ddavidjonker@outlook.com>
2024-08-18 20:57:15 +0200

    ongoing

--- HiveGame/Models/Moves/Move.cs
+++ HiveGame/Models/Moves/Move.cs
@@ -1,5 +1,4 @@
-﻿using System.Security.Cryptography;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using Hive.Core.Enums;
 using Hive.Core.Exceptions;
 using Hive.Core.Models;
